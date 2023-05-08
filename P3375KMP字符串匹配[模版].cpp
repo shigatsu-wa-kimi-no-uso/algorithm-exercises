4b6b3d0f037ecfa1e8de57a4c8b8364f3a5a1eaf
kiminouso <1790226429@qq.com>
2023-05-08 21:41:20 +0800

    更新题目代码:P3375KMP字符串匹配[模版]

--- P3375KMP字符串匹配[模版].cpp
+++ P3375KMP字符串匹配[模版].cpp
@@ -2,9 +2,9 @@
 #include<string>
 using namespace std;
 
-//P3375 【模板】KMP字符串匹配
 
 int nextj[(int)1E6 + 10];
+int nextval[(int)1E6 + 10];
 
 void init_next(const string& t) {
 	int len = t.length();
@@ -22,13 +22,22 @@ void init_next(const string& t) {
 	}
 }
 
-
+void init_nextval(const string& t) {
+	int len = t.length();
+	for (int j = 2; j <= len; ++j) {
+		int k = j ;
+		do {
+			k = nextj[k];
+		} while (t[j] == t[k] && k!=0);
+		nextval[j] = k;
+	}
+}
 void KMP_find(const string& s, const string& t) {
 	int slen = s.length();
 	int tlen = t.length();
 	int j = 0;
 	for (int i = 0; i < slen; i = (j == 0 ? i + 1 : i)) {
-		for (j = nextj[j]; j < tlen && s[i] == t[j] ; ++i, ++j);
+		for (j = nextval[j]; j < tlen && s[i] == t[j] ; ++i, ++j);
 		if (j == tlen) {
 			cout << i - tlen + 1 << '\n';
 		}
@@ -43,6 +52,7 @@ int main() {
 	string s,t;
 	cin >> s >> t;
 	init_next(t);
+	init_nextval(t);
 	KMP_find(s,t);
 	int tlen = t.length();
 	for (int i = 1; i <= tlen; ++i) {
